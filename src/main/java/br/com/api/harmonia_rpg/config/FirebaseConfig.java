7843feeffc570b979334d36c337f43a6e56b2e6d
MatheusGmota <livematgreg@gmail.com>
2025-12-05 15:55:28 -0300

    feat: add dependência do firebase

---
+++ src/main/java/br/com/api/harmonia_rpg/config/FirebaseConfig.java
@@ -0,0 +1,17 @@
+package br.com.api.harmonia_rpg.config;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public Firestore firestore(){
+        if(FirebaseApp.getApps().isEmpty()) FirebaseApp.initializeApp();
+        return FirestoreClient.getFirestore();
+    }
+}
