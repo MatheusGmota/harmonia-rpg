32a1f2cd188a14ca30bd002f999c72e40bb97a69
MatheusGmota <livematgreg@gmail.com>
2025-12-05 14:58:12 -0300

    first commit

---
+++ src/main/java/br/com/api/harmonia_rpg/HarmoniaRpgApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.harmonia_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniaRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmoniaRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/harmonia_rpg/HarmoniaRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.harmonia_rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniaRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
