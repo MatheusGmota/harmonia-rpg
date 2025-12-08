c54261efb841c288d64876cba1988e0e0d2d4119
MatheusGmota <livematgreg@gmail.com>
2025-12-08 19:25:21 -0300

    feat: adicionado dependências do spring security e validation

--- src/main/java/br/com/api/harmonia_rpg/domain/entities/Usuario.java
+++ src/main/java/br/com/api/harmonia_rpg/domain/entities/Usuario.java
@@ -1,16 +1,24 @@
 package br.com.api.harmonia_rpg.domain.entities;
 
+import br.com.api.harmonia_rpg.domain.enums.TipoUsuario;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.cloud.firestore.annotation.ServerTimestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails  {
 
     @DocumentId
     private String id;
@@ -19,6 +27,36 @@ public class Usuario {
 
     private String senha;
 
+    private TipoUsuario tipoUsuario;
+
     @ServerTimestamp
     private Timestamp criadoEm;
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.tipoUsuario == TipoUsuario.MESTRE) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return nomeUsuario;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
 }
