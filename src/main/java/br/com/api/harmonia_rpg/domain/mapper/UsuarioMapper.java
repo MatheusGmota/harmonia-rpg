b270da503fd17a7e124434973a1bf8f1c179e0a9
MatheusGmota <livematgreg@gmail.com>
2025-12-10 19:34:39 -0300

    feat: criado mapper para usuário

---
+++ src/main/java/br/com/api/harmonia_rpg/domain/mapper/UsuarioMapper.java
@@ -0,0 +1,27 @@
+package br.com.api.harmonia_rpg.domain.mapper;
+
+import br.com.api.harmonia_rpg.domain.dtos.CadastroRequestDTO;
+import br.com.api.harmonia_rpg.domain.dtos.UsuarioRequestDTO;
+import br.com.api.harmonia_rpg.domain.entities.Usuario;
+
+public class UsuarioMapper {
+
+    private UsuarioMapper() {}
+
+    private static Usuario baseUsuario(String nomeUsuario, String senha) {
+        Usuario u = new Usuario();
+        u.setNomeUsuario(nomeUsuario);
+        u.setSenha(senha);
+        return u;
+    }
+
+    public static Usuario from(CadastroRequestDTO request) {
+        Usuario u = baseUsuario(request.nomeUsuario(), request.senha());
+        u.setTipoUsuario(request.tipoUsuario());
+        return u;
+    }
+
+    public static Usuario from(UsuarioRequestDTO request) {
+        return baseUsuario(request.nomeUsuario(), request.senha());
+    }
+}
