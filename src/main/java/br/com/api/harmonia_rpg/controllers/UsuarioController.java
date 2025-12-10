17bbd84548d3bc1d59b38f8c4ebddea06d0fb683
MatheusGmota <livematgreg@gmail.com>
2025-12-10 19:40:33 -0300

    fix: implementado repository e refatorado controller

--- src/main/java/br/com/api/harmonia_rpg/controllers/UsuarioController.java
+++ src/main/java/br/com/api/harmonia_rpg/controllers/UsuarioController.java
@@ -1,48 +1,46 @@
 package br.com.api.harmonia_rpg.controllers;
 
-import br.com.api.harmonia_rpg.domain.entities.Usuario;
+import br.com.api.harmonia_rpg.domain.dtos.CadastroRequestDTO;
+import br.com.api.harmonia_rpg.domain.dtos.CadastroResponseDTO;
+import br.com.api.harmonia_rpg.domain.dtos.UsuarioRequestDTO;
+import br.com.api.harmonia_rpg.domain.dtos.UsuarioResponseDTO;
 import br.com.api.harmonia_rpg.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/usuario")
+@CrossOrigin("*")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> get(@PathVariable("id") String id) {
-        Usuario res = service.obterUsuario(id);
-        if (res == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum usuário encontrado");
-        return ResponseEntity.ok(res);
+    public ResponseEntity<Object> getById(@PathVariable("id") String id) {
+        UsuarioResponseDTO resposta = service.obterUsuarioPorId(id);
+        return ResponseEntity.ok(resposta);
     }
 
     @PostMapping
-    public ResponseEntity<Object> create(@RequestBody Usuario usuario) {
-        Object res = service.criarUsuario(usuario);
-        if (res == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum usuário encontrado");
-        return ResponseEntity.ok(res);
+    public ResponseEntity<Object> create(@Valid @RequestBody CadastroRequestDTO dto) {
+        CadastroResponseDTO resposta = service.registrarUsuario(dto);
+        return ResponseEntity.ok(resposta);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> update(@PathVariable("id") String id, @RequestBody Usuario usuario) {
-        String res = service.editarUsuario(id, usuario);
-        if (res == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum usuário encontrado");
-        return ResponseEntity.ok(res);
+    public ResponseEntity<Object> update(@PathVariable("id") String id, @Valid @RequestBody UsuarioRequestDTO dto) {
+        Map<String, Object> resposta = service.editarUsuario(id, dto);
+        return ResponseEntity.ok(resposta);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> delete(@PathVariable("id") String id) {
-        String res = service.deletarUsuario(id);
-        if (res == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum usuário encontrado");
-        return ResponseEntity.ok(res);
+        Map<String, Object> resposta = service.deletarUsuario(id);
+        return ResponseEntity.ok(resposta);
     }
 }
--- src/main/java/br/com/api/harmonia_rpg/service/UsuarioService.java
+++ src/main/java/br/com/api/harmonia_rpg/service/UsuarioService.java
@@ -1,14 +1,22 @@
 package br.com.api.harmonia_rpg.service;
 
+import br.com.api.harmonia_rpg.domain.dtos.CadastroRequestDTO;
+import br.com.api.harmonia_rpg.domain.dtos.CadastroResponseDTO;
+import br.com.api.harmonia_rpg.domain.dtos.UsuarioRequestDTO;
+import br.com.api.harmonia_rpg.domain.dtos.UsuarioResponseDTO;
 import br.com.api.harmonia_rpg.domain.entities.Usuario;
-import com.google.api.core.ApiFuture;
+import br.com.api.harmonia_rpg.domain.exceptions.NotFoundException;
+import br.com.api.harmonia_rpg.domain.exceptions.UserAlreadyExistsException;
+import br.com.api.harmonia_rpg.domain.mapper.UsuarioMapper;
+import br.com.api.harmonia_rpg.repositories.UsuarioRepository;
 import com.google.cloud.firestore.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -16,23 +24,24 @@ import java.util.concurrent.ExecutionException;
 public class UsuarioService {
 
     @Autowired
+    private UsuarioRepository db;
+
+    @Autowired
     private Firestore firestore;
 
-    public String criarUsuario(Usuario usuario) {
+    public CadastroResponseDTO registrarUsuario(CadastroRequestDTO requisicao) {
         try {
-            // validar se usuario já existe no banco de dados
-            List<QueryDocumentSnapshot> documents = firestore
-                    .collection("usuarios")
-                    .whereEqualTo("nomeUsuario", usuario.getNomeUsuario())
-                    .get()
-                    .get().getDocuments();
-            if (!documents.isEmpty()) {
-                return "Usuário já cadastrado.";
-            }
+            Usuario usuario = UsuarioMapper.from(requisicao);
 
-            ApiFuture<DocumentReference> usuarioReference = firestore.collection("usuarios").add(usuario);
+            // Valida se nomeUsuario é existente
+            validarNomeUsuario(usuario.getNomeUsuario(), "Usuário já cadastrado.");
 
-            return "Documento salvo, idUsuario: " + usuarioReference.get().getId();
+            // Encriptando senha
+            usuario.setSenha(encriptarSenha(usuario.getSenha()));
+
+            Usuario salvar = db.salvar(usuario).get().toObject(Usuario.class);
+
+            return new CadastroResponseDTO(salvar.getId(), salvar.getNomeUsuario(), salvar.getCriadoEm());
         } catch (InterruptedException | ExecutionException e) {
             log.error(e.getMessage());
             throw new RuntimeException(e);
@@ -40,57 +49,45 @@ public class UsuarioService {
 
     }
 
-    public Usuario obterUsuario(String id) {
+    public UsuarioResponseDTO obterUsuarioPorId(String id) {
         try {
-            ApiFuture<DocumentSnapshot> usuario = firestore.collection("usuarios").document(id).get();
-            if (!usuario.get().exists()) {
-                log.error("Nenhum usuário encontrado");
-                return null;
-            }
+            DocumentSnapshot usuarioPorId = db.obterUsuarioPorId(id);
 
-            return usuario.get().toObject(Usuario.class);
-        } catch (InterruptedException | ExecutionException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public String editarUsuario(String id, Usuario usuario) {
-        try {
-            ApiFuture<DocumentSnapshot> usuarios = firestore.collection("usuarios").document(id).get();
-
-            if (!usuarios.get().exists()) {
+            if (!usuarioPorId.exists()) {
                 log.error("Não existe usuário para id: {}", id);
-                return null;
+                throw new NotFoundException("Usuário não encontrado");
             }
+
+            Usuario usuario = usuarioPorId.toObject(Usuario.class);
+
+            return new UsuarioResponseDTO(usuario.getId(), usuario.getNomeUsuario(), usuario.getTipoUsuario(), usuario.getCriadoEm());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, Object> editarUsuario(String id, UsuarioRequestDTO requisicao) {
+        try {
+            Usuario usuario = UsuarioMapper.from(requisicao);
+
+            obterUsuarioPorId(id); // Faz chamada apenas para validar existência do usuário pelo ID
+
+            validarNomeUsuario(usuario.getNomeUsuario(), "Nome de usuário já utilizado");
 
             HashMap<String, Object> map = new HashMap<>();
             map.put("nomeUsuario", usuario.getNomeUsuario());
-            map.put("senha", usuario.getSenha());
 
-            firestore.collection("usuarios").document(id).update(map);
-
-            return "Usuário atualizado em: " + usuarios.get().getUpdateTime();
-        } catch (InterruptedException | ExecutionException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public String deletarUsuario(String id) {
-        try {
-            ApiFuture<DocumentSnapshot> usuarios = firestore.collection("usuarios").document(id).get();
-
-            if (!usuarios.get().exists()) {
-                log.error("Não existe usuário para id: {}", id);
-                return null;
+            if (usuario.getSenha() != null) {
+                usuario.setSenha(encriptarSenha(usuario.getSenha())); // Encriptando senha
+                map.put("senha", usuario.getSenha());
             }
 
-            ApiFuture<WriteResult> deletado = firestore.collection("usuarios").document(id).delete();
+            WriteResult atualizar = db.atualizar(id, map);
 
-            return "Usuário deletado em: " + deletado.get().getUpdateTime();
+            log.info("Usuário id:{} atualizado em: {}", id, atualizar.getUpdateTime());
+
+            return Map.of("id", id, "atualizadoEm", atualizar.getUpdateTime());
         } catch (InterruptedException | ExecutionException e) {
             log.error(e.getMessage());
             throw new RuntimeException(e);
@@ -98,5 +95,31 @@ public class UsuarioService {
 
     }
 
+    public Map<String, Object> deletarUsuario(String id) {
+        try {
+            obterUsuarioPorId(id); // Faz chamada apenas para validar existência do usuário pelo ID
+
+            WriteResult atualizar = db.deletar(id);
+
+            log.info("Usuário id:{} deletado em: {}", id, atualizar.getUpdateTime());
+
+            return Map.of("message", "Deletado com sucesso", "atualizadoEm", atualizar.getUpdateTime());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validarNomeUsuario(String nomeUsuario, String mensagemErro)  {
+        Usuario existente = db.buscarPorNomeUsuario(nomeUsuario); // Caso nomeUsuário não exista irá retornar nulo
+
+        if (existente != null) {
+            throw new UserAlreadyExistsException(mensagemErro);
+        }
+    }
+
+    private String encriptarSenha(String senha) {
+        return new BCryptPasswordEncoder().encode(senha);
+    }
 
 }
