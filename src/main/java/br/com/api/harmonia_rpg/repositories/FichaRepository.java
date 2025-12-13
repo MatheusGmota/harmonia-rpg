50081e59896c4afb31710673c84927126a964bd4
MatheusGmota <livematgreg@gmail.com>
2025-12-13 16:18:05 -0300

    feat: criado repository para Ficha

---
+++ src/main/java/br/com/api/harmonia_rpg/repositories/FichaRepository.java
@@ -0,0 +1,65 @@
+package br.com.api.harmonia_rpg.repositories;
+
+import br.com.api.harmonia_rpg.domain.dtos.FichaUsuarioDTO;
+import br.com.api.harmonia_rpg.domain.entities.Ficha;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Repository
+public class FichaRepository {
+
+    private static final String COLLECTION_NAME = "fichas";
+
+    @Autowired
+    public Firestore db;
+
+    private CollectionReference getCollection() {
+        return db.collection(COLLECTION_NAME);
+    }
+
+    public List<FichaUsuarioDTO> obterFichasDoUsuario(String idUsuario) throws ExecutionException, InterruptedException {
+
+        ApiFuture<QuerySnapshot> fichas = getCollection()
+                .whereEqualTo("idUsuario", idUsuario)
+                .get();
+
+        return fichas
+                .get()
+                .getDocuments()
+                .stream().map(f -> {
+                    Ficha o = f.toObject(Ficha.class);
+
+                    return FichaUsuarioDTO.from(o);
+                }).toList();
+    }
+
+    public DocumentSnapshot obterFichaPorId(String id) throws ExecutionException, InterruptedException {
+        ApiFuture<DocumentSnapshot> fichas = getCollection().document(id).get();
+
+        return fichas.get();
+    }
+
+    public ApiFuture<DocumentSnapshot> adicionar(Ficha ficha) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = getCollection().add(ficha).get();
+        return documentReference.get();
+    }
+
+    public WriteResult atualizar(String id, Ficha ficha) throws ExecutionException, InterruptedException {
+        return getCollection()
+                .document(id)
+                .set(ficha, SetOptions.merge())
+                .get();
+    }
+
+    public WriteResult deletar(String id) throws ExecutionException, InterruptedException {
+        return getCollection()
+                .document(id)
+                .delete().get();
+    }
+}
